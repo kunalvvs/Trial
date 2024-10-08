684c5f1294e43a5b3149050cf28d97749da7bdab
kunalvvs <kunalvvs@gmail.com>
2024-10-08 11:48:37 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Second from './Second'
+
+
+
+function App() {
+  return <>
+  
+    <h1 className='h1 p-10 '>First React App </h1>
+    <h3>It is description </h3>
+   <Second/>
+  
+  </>
+  };
+
+export default App
